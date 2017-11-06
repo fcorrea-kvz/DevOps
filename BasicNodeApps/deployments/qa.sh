bf1e18b9690fc16ce71104c12bdf77510fecd44f
Felipe Correa <felipe.correa.santis@gmail.com>
2017-11-06 09:47:05 -0300

    fix qa.sh

--- BasicNodeApps/deployments/qa.sh
+++ BasicNodeApps/deployments/qa.sh
@@ -4,6 +4,6 @@ pwd
 ls -l
 echo "Deploying..."
 echo "TOKEN: $NOW_TOKEN"
-URL=$(now --docker ../ -t $NOW_TOKEN)
+URL=$(now --docker -t $NOW_TOKEN $HOME/DevOps/BasicNodeApps/)
 echo "running acceptance on $URL"
 curl --silent -L $URL 
